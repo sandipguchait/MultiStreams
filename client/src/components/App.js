c481668b4b373da91aa363ee2f021e209d5f3a0f
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-12 15:47:30 +0530

    completed routes using Browser router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'; 
 
-class App extends Component {
-  render() {
-    return (
-      <div>APP</div>
-    );
-  }
+//components
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/streamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+ 
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList}/>
+          <Route path="/streams/new"  component={StreamCreate}/>
+          <Route path="/streams/edit"  component={StreamEdit}/>
+          <Route path="/streams/delete"  component={StreamDelete}/>
+          <Route path="/streams/show"  component={StreamShow}/>
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App;
