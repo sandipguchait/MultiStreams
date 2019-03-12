2da67c772742fa467cdb2e3c309aa2cab873482d
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-12 20:12:04 +0530

    fixing onclick properties and adding react-redux, redux to project

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -27,11 +27,11 @@ class GoogleAuth extends Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get()})
     }
 
-    onSignIn = () => {
+    onSignInClick = () => {
         this.auth.signIn();
     }
 
-    onSignOut = () => {
+    onSignOutClick = () => {
         this.auth.signOut();
     }
     // renders the UI with display of buttons
@@ -40,14 +40,14 @@ class GoogleAuth extends Component {
             return null
         } else if (this.state.isSignedIn ) {
             return (
-                <button onClick={()=> this.onSignOut()} className=" ui red google button">
+                <button onClick={()=> this.onSignOutClick()} className=" ui red google button">
                     <i className="google icon"/>
                     Sign Out
                 </button>
             )
         } else {
             return (
-                <button onClick={()=>this.onSignIn()} className="ui blue google button">
+                <button onClick={()=>this.onSignInClick()} className="ui blue google button">
                     <i className=" google icon"/>
                     Sign In With Google
                 </button>
