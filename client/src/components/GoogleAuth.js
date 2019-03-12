1af1e0f7b4dda208decc0d1c6b845dead7a2a2c8
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-12 19:56:31 +0530

    adding comments to GoogleAuth component

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
 
+    // initial state to hold sign in value
     state = {
         isSignedIn: null
     }
+
+    // when ever page renders the client connects with the google OAuth feature
     componentDidMount() {
         window.gapi.load('client:auth2', ()=> {
             window.gapi.client.init({
                 clientId: '267461606910-tb7g2t4p0vop6jtr5tcdg5l23129i8ru.apps.googleusercontent.com',
                 scope: 'email'
             }).then(()=>{
+                //on giving the configurations above, we get some callback object
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({ isSignedIn: this.auth.isSignedIn.get()})
+                //listening for any change on oath signin/signout feature
                 this.auth.isSignedIn.listen(this.onAuthChange)
             })
         });
     }
-
+    //this renders the authentication state to update as a realtime basis
     onAuthChange = () => {
         this.setState({ isSignedIn: this.auth.isSignedIn.get()})
     }
@@ -29,7 +34,7 @@ class GoogleAuth extends Component {
     onSignOut = () => {
         this.auth.signOut();
     }
-
+    // renders the UI with display of buttons
     renderAuthButton = () => {
         if(this.state.isSignedIn === null ) {
             return null
