64da9f1135343238a57ff7afe8cf19eb422f1998
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-14 18:12:40 +0530

    customizing form labels and ui

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -5,17 +5,20 @@ import { Field, reduxForm } from 'redux-form';
 class StreamCreate extends Component {
     // Rendering form data from redux-form
     // input props is provided by redux-form
-    renderInput = ({ input}) => {
+    renderInput = ({ input, label }) => {
         return (
-            <input {...input} />
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
         )
     }
 
     render() {
         return (
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description"  component={this.renderInput}/>
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title"/>
+                <Field name="description"  component={this.renderInput} label="Enter description"/>
             </form>
         );
     }
