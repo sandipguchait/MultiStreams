59e0415cea55ad16d12076da800ea17e5bd940d7
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-14 18:37:34 +0530

    handling form submission

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,29 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-    // Rendering form data from redux-form
-    // input props is provided by redux-form
-    renderInput = ({ input, label }) => {
-        return (
-            <div className="field">
-                <label>{label}</label>
-                <input {...input} />
-            </div>
-        )
-    }
+  // Rendering form data from redux-form
+  // input props is provided by redux-form
+  renderInput = ({ input, label }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  };
 
-    render() {
-        return (
-            <form className="ui form">
-                <Field name="title" component={this.renderInput} label="Enter Title"/>
-                <Field name="description"  component={this.renderInput} label="Enter description"/>
-            </form>
-        );
-    }
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
+
+  render() {
+    return (
+      //this.props.handleSubmit is supplied as props from redux-form 
+      //this.onSubmit() is the helper function
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
 }
 
 export default reduxForm({
-    form:'streamCreate'
+  form: "streamCreate"
 })(StreamCreate);
