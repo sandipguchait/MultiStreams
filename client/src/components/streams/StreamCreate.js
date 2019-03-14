9184f562e460c266871d54a5c2a7e31fdfceb6b2
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-14 19:48:02 +0530

    showing errors on touched and delect

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -10,10 +10,25 @@ class StreamCreate extends Component {
       <div className="field">
         <label>{label}</label>
         <input {...input} />
-        <div>{meta.error}</div>
+        {/* meta is the validate error property that we get from redux from validate function below */}
+        {this.renderError(meta)}
       </div>
     );
   };
+
+  renderError = ({ error, touched }) => {
+      //touched is a property on reduxform that shows that is a filed selected or not
+      // if a field is selected and there is error then show the error else not
+    if(touched && error ) {
+        return(
+            <div className="ui error message">
+                <div className="header">
+                    {error}
+                </div>
+            </div>
+        )
+    }
+  }
 
   onSubmit = formValues => {
     console.log(formValues);
