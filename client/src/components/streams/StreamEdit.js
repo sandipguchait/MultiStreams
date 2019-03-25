e000f1fc28c909ce3dfe48a595792506d90cfb17
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-26 00:09:55 +0530

    calling fetchStream action creator and passing the stream id from react-router-dom params

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {fetchStream} from '../../actions/index';
 
-const StreamEdit = (props) => {
+class StreamEdit extends React.Component {
+
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render(){
     return (
         <div>StreamEdit</div>
     );
+   }
 };
 
-export default StreamEdit;
+// MapStateToprops has 2 arguments one is State and another is the PROPS 
+//thats is passed to the component
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
