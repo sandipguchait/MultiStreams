4e293bcb6d39d10575eaefc5652248d70d3e0bae
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-25 21:27:39 +0530

    creating a custom history prop on Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'; 
+import { Router, Route } from 'react-router-dom'; 
 
 //components
 import StreamCreate from './streams/StreamCreate';
@@ -8,11 +8,13 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import Header from './Header';
- 
+import history from '../history';
+
+
 const App = () => {
   return (
     <div className="ui container" style={{ marginTop:'10px'}}>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header/>
           <Route path="/" exact component={StreamList}/>
@@ -21,7 +23,7 @@ const App = () => {
           <Route path="/streams/delete"  component={StreamDelete}/>
           <Route path="/streams/show"  component={StreamShow}/>
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 }
