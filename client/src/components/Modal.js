cd4bd098809de22a05e5e573a5abcd75d89679fe
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-27 16:35:24 +0530

    adding history to push a person to different route on outside modal click + using stopPropagation() to stop selfclicking of div's

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import history from '../history';
 
 // Creating a React portal to crearte a Modal
 const Modal = (props) => {
     return (
         ReactDOM.createPortal(
-            <div className="ui dimmer modals visible active">
-              <div className="ui standard modal visible active">
-                jrbjbagbrkjgbeg 
+            <div className="ui dimmer modals visible active" onClick={()=>history.push('/')}>
+              <div className="ui standard modal visible active" onClick={(e)=> e.stopPropagation()}>
+                <div  className="header">Delete Stream</div>
+                <div className="content">
+                Are you sure you want to delete the stream
+                </div>
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
               </div>  
             </div>,
             document.querySelector('#modal')
