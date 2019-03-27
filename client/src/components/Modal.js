1de71625c3b30a330e08f7dfe0981a99052855f6
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-27 19:14:29 +0530

    reusing the onClick property on Modal by getting just props

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import history from '../history';
+
 
 // Creating a React portal to crearte a Modal
 const Modal = (props) => {
     return (
         ReactDOM.createPortal(
-            <div className="ui dimmer modals visible active" onClick={()=>history.push('/')}>
+            <div className="ui dimmer modals visible active" onClick={props.onDismiss}>
               <div className="ui standard modal visible active" onClick={(e)=> e.stopPropagation()}>
                 <div  className="header">{props.title}</div>
                 <div className="content">
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
 
@@ -16,6 +17,7 @@ const StreamDelete = () => {
                 title='DeleteStream'
                 content="Are you sure you want to delete the stream ?"
                 actions={actions}
+                onDismiss={()=> history.push('/')}
             />
         </div>
     );
