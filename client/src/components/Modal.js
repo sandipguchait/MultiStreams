8e58ced653ea925397b92e34ad47d76d6ef4d7d2
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-27 17:12:51 +0530

    making the Modal highly reusable and getting vales only via props

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -8,13 +8,12 @@ const Modal = (props) => {
         ReactDOM.createPortal(
             <div className="ui dimmer modals visible active" onClick={()=>history.push('/')}>
               <div className="ui standard modal visible active" onClick={(e)=> e.stopPropagation()}>
-                <div  className="header">Delete Stream</div>
+                <div  className="header">{props.title}</div>
                 <div className="content">
-                Are you sure you want to delete the stream
+                {props.content}
                 </div>
                 <div className="actions">
-                    <button className="ui primary button">Delete</button>
-                    <button className="ui button">Cancel</button>
+                    {props.actions}
                 </div>
               </div>  
             </div>,
